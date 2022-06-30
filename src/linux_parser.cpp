64d6b920423df56a70d88e8621a4fc1840da1939
Jairo <jjairo26@hotmail.com>
2022-06-30 12:48:19 +0200

    Fixed overloaded 'less than'. Return 0.0 instead of empty string for zombie processes

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -269,7 +269,7 @@ string LinuxParser::Ram(int pid) {
     }
   }
 
-  return ram;
+  return "0.0";
 }
 
 // TODO: Read and return the user ID associated with a process -> DONE
--- src/process.cpp
+++ src/process.cpp
@@ -41,5 +41,6 @@ long int Process::UpTime() { return LinuxParser::UpTime(Pid()); }
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
 bool Process::operator<(Process const& a) const {
-  return (this->CpuUtilization() < a.CpuUtilization());
+  // the other way around to sort in descending order later
+  return (a.CpuUtilization() < this->CpuUtilization()); 
 }
